d3607ef30b7362b968cd51f457a1c574df4560b7
LilithaThengwa <lilithathengwa@gmail.com>
2024-04-13 18:10:23 +0200

    main_bp

--- app.py
+++ app.py
@@ -20,7 +20,6 @@ connection_string = os.environ.get("LOCAL_DATABASE_URL")
 app.config["SECRET_KEY"] = os.environ.get("FORM_SECRET_KEY")
 print(os.environ.get("FORM_SECRET_KEY"))
 app.config["SQLALCHEMY_DATABASE_URI"] = connection_string
-# db = SQLAlchemy(app)
 db.init_app(app)
 login_manager.init_app(app)
 
@@ -28,66 +27,10 @@ login_manager.init_app(app)
 def load_user(user_id):
     return Customer.query.get(user_id)
 
-class LegalInsuranceEstimateForm(FlaskForm):
-    cover_amount = IntegerField("Cover Amount Required", validators=[DataRequired()])
-    cover_type = SelectField("Level of Cover Required", choices=[
-        ("", "Select Cover Level"),
-        ("Basic Coverage", "Basic Coverage"),
-        ("Standard Coverage", "Standard Coverage"),
-        ("Comprehensive Coverage", "Comprehensive Coverage"),
-        ("Customized Coverage", "Customized Coverage")
-    ], validators=[DataRequired()])
-    criminal_record = BooleanField("Criminal record")
-    tried = BooleanField("Have been tried and found not guilty")
-    arrested = BooleanField("Have been arrested or been the subject of criminal investigation")
-    none = BooleanField("Never been arrested or been the subject of criminal investigation")
-    government_official = SelectField("Are you, or any of your close relatives high ranking government officials?", choices=[
-        ("", "Select"),
-        ("Yes", "Yes"),
-        ("No", "No")
-    ], validators=[DataRequired()])
-    age = IntegerField("Age", validators=[DataRequired()])
-    occupation = StringField("Occupation", validators=[DataRequired()])
-    marital_status = SelectField("Marital status", choices=[
-        ("", "Select Marital status"),
-        ("Never Married", "Never Married"),
-        ("Community of property", "Married in Community of Property"),
-        ("Out of community", "Married out of Community of Property"),
-        ("Divorced", "Divorced")
-    ], validators=[DataRequired()])
-    submit = SubmitField("Get Estimate")
-
-# =============================================================================================================================
-
-@app.context_processor
-def inject_items():
-    items = [policy for policy in PolicyType.query.all()] 
-    return dict(nav_dopdown_items=items)
-
 @app.context_processor
 def inject_user_role():
     user_role = session.get("user_role")
     return dict(user_role=user_role)
-
-@app.route("/")
-def home():
-    return render_template("home.html", policyTypes=PolicyType.query.all())
-
-# =============================================================================================================================
-# =============================================================================================================================
-
-@app.route("/policy/<id>")
-def policy(id):
-    form = LegalInsuranceEstimateForm()
-    policy_type = PolicyType.query.get(id)
-    if policy:
-        return render_template("policy.html", policy=policy_type, form=form)
-    else:
-        return f"Policy not found for ID: {id}", 404 
-    
-@app.route("/quote")
-def quote():
-    return render_template("quote.html")
     
 if __name__ == "__main__":
     app.run(debug = True)
@@ -101,7 +44,7 @@ from routes.crud_bp import crud_bp
 app.register_blueprint(about_bp, url_prefix="/about")
 app.register_blueprint(user_bp, url_prefix="/user")
 app.register_blueprint(admin_bp, url_prefix="/admin")
-app.register_blueprint(main_bp, url_prefix="/main")
+app.register_blueprint(main_bp)
 app.register_blueprint(crud_bp, url_prefix="/raw-crud")
 
 # try:
